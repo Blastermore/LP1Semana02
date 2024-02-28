70d0c7c78a63709e8cfb77dce3ac0c829d4e71fc
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-28 20:12:49 +0000

    Expanded SmoothOperator project to include: Division of n by 2; Shift left by 3; Xor with 5; and check if n > 10, Adding a print for each of these operations

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -13,7 +13,18 @@ namespace SmoothOperator
 
             Console.WriteLine(--n_pro);
             Console.WriteLine(++n_pro);
+            
+            int n_pro_div = n_pro/2;
+            Console.WriteLine(n_pro_div);
 
+            int n_pro_shift = n_pro << 3;
+            Console.WriteLine(n_pro_shift);
+
+            int n_pro_xor = n_pro ^ 5;
+            Console.WriteLine(n_pro_xor);
+
+            bool n_pro_comp = n_pro > 10;
+            Console.WriteLine(n_pro_comp);
 
         }
     }
