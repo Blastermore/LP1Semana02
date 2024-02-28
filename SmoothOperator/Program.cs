1423c1911ab5fbbd7a35314937ed08224d92dc4b
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-28 19:54:19 +0000

    Added request for a positive int input; Converted to sbyte and stored that way; Decremented and incrmeeted said number

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um número inteiro não-negativo:");
+            string n = Console.ReadLine();
+
+            sbyte n_pro  = sbyte.Parse(n);
+
+            Console.WriteLine(--n_pro);
+            Console.WriteLine(++n_pro);
+
+
+        }
+    }
+}
