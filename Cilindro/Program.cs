04fd9a0db8f68730ec2db4c569f3de30259d5921
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-28 20:36:50 +0000

    Added request for a height and radius input; Converted and stored thos evalues in floats; Stored the MathF.PI constant in a float variable; Added a print for the future values of the volume and surface area

---
+++ Cilindro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Um valor para a altura do cilindro");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Insira um valor para o raio do cilindro");
+            string r = Console.ReadLine();
+
+            float a_pro = float.Parse(a);
+            float r_pro = float.Parse(r);
+            float PI = MathF.PI;
+
+            float V = MathF.PI * MathF.Pow(r_pro,2) * a_pro;
+            float Sa = 2 * PI * r_pro * (r_pro + a_pro);
+
+            Console.WriteLine($"O volume do cilindro é. {V}");
+            Console.WriteLine($"A área de superfície do cilindro é: {Sa}");
+        }
+    }
+}
