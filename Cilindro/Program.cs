3c24f1f7ca5e114b86226f70020634d86e968271
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-28 20:37:51 +0000

    Added the formulas for volume and surface area of the clinder and stored each of its valus in a float variable to be printed in a formatted string

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -16,7 +16,7 @@ namespace Cilindro
             float r_pro = float.Parse(r);
             float PI = MathF.PI;
 
-            float V = MathF.PI * MathF.Pow(r_pro,2) * a_pro;
+            float V = PI * MathF.Pow(r_pro,2) * a_pro;
             float Sa = 2 * PI * r_pro * (r_pro + a_pro);
 
             Console.WriteLine($"O volume do cilindro é. {V}");
