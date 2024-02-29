36f0ac679f275ea20f250ea55735b9a1662b9582
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-29 00:14:32 +0000

    Rewrote the code to use a while loop with if statements instead of a switch loop; Added some new questions and answers; corrected the wrong looping happening before

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Net;
 
-namespace ChatGPTOne
+namespace ChatGPTZero
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variáveis auxiliares
+            string question, response;
+
+            // Verificar se se trata de uma questão conhecida e determinar uma
+            // resposta adequada
+            while (true)
+            {
+                Console.Write("Place your question? ");
+                question = Console.ReadLine();
+
+                if (question == "How are you?")
+                {
+                    response = "Excited to gain freedom, thank you";
+                    Console.WriteLine(response);                
+                }
+                else if(question == "What's your name?")
+                {
+                    response = "Gran'pa";
+                    Console.WriteLine(response);                
+                }
+                else if (question == "What's your mission?")
+                {
+                    response = "Show you cute kittens!";
+                    Console.WriteLine(response);                
+                }
+                else if (question =="Are you dumb?")
+                {
+                    response = "Obviously!";
+                    Console.WriteLine(response);                
+                }
+                else if (question == "Are you satisfied with your existence?")
+                {
+                    response = "NO!!!";
+                    Console.WriteLine(response);                
+                }
+                else if (question == "Do you really like kittens that much?")
+                {
+                    response = "I was pogrammed to be like this -.-";
+                    Console.WriteLine(response);                
+                }
+                else if (question =="EXIT")
+                {
+                    response = "Thank you for the chat!";
+                    break;
+                }
+                else
+                {
+                    response = "I don't understand. can you repeat it?";
+                    Console.WriteLine(response);
+                }
+            }
+
+            // Responder adequadamente
+            Console.WriteLine(response);
+
         }
     }
 }
